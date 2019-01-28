374847497f3ec2c4258aea483087c16361bd514b
Hoang <hoang.nn@big-labs.com>
2019-01-28 22:57:47 +0700

    Update api to upload image.

--- MozoSDK/Classes/Modules/ImageService/Business Logic/Manager/ImageServiceApiManager.swift
+++ MozoSDK/Classes/Modules/ImageService/Business Logic/Manager/ImageServiceApiManager.swift
@@ -13,7 +13,7 @@ let IMAGE_SERVICE_API_PATH = "/image"
 public extension ApiManager {
     public func getUrlToUploadImage() -> Promise<String> {
         return Promise { seal in
-            let url = Configuration.BASE_STORE_URL + IMAGE_SERVICE_API_PATH + "/url-for-upload-url"
+            let url = Configuration.BASE_STORE_URL + IMAGE_SERVICE_API_PATH + "/url-for-binary"
             self.execute(.get, url: url)
                 .done { json -> Void in
                     // JSON info
@@ -38,30 +38,39 @@ public extension ApiManager {
             Alamofire.upload(multipartFormData: { (multipartFormData) in
                 for image in images {
                     if let imgData = UIImagePNGRepresentation(image) {
-                        multipartFormData.append(imgData, withName: "\(Date().timeIntervalSince1970).png", mimeType: "image/png")
+                        let imageSize = imgData.count
+                        print("Upload image with size in KB: \(Double(imageSize) / 1024.0)")
+                        multipartFormData.append(imgData, withName: "", fileName: "\(Date().timeIntervalSince1970).png", mimeType: "image/*")
                     }
                 }
             }, to: url, encodingCompletion: { encodingResult in
                 switch encodingResult {
                 case .success(let upload, _, _):
                     upload.responseJSON { response in
-                        print("Finish request to upload image, response: \(response)")
-                        guard let json = response.result.value as? [String: Any] else {
-                            return seal.reject(AFError.responseValidationFailed(reason: .dataFileNil))
-                        }
-                        self.handleApiResponseJSON(json, url: url).done({ (jsonData) in
-                            let jobj = JSON(jsonData)
-                            let items = jobj.array ?? []
-                            var values : [String] = []
-                            for item in items {
-                                if let string = item[RESPONSE_TYPE_VALUE_KEY].string {
-                                    values.append(string)
-                                }
+                        switch response.result {
+                        case .success(let json):
+                            print("Finish request to upload image, json: \(json)")
+                            guard let json = json as? [String: Any] else {
+                                return seal.reject(AFError.responseValidationFailed(reason: .dataFileNil))
                             }
-                            seal.fulfill(values)
-                        }).catch({ (error) in
-                            seal.reject(error)
-                        })
+                            self.handleApiResponseJSON(json, url: url).done({ (jsonData) in
+                                let jobj = JSON(jsonData)
+                                let items = jobj[RESPONSE_TYPE_ARRAY_KEY].array ?? []
+                                var values : [String] = []
+                                for item in items {
+                                    if let string = item[RESPONSE_TYPE_VALUE_KEY].string {
+                                        values.append(string)
+                                    }
+                                }
+                                seal.fulfill(values)
+                            }).catch({ (error) in
+                                seal.reject(error)
+                            })
+                        case .failure(let error):
+                            print("Request failed with error: \(error.localizedDescription), url: \(url), detail: \(self.getErrorDetailMessage(responseData: response.data))")
+                            let connectionError = self.checkResponse(response: response, error: error)
+                            seal.reject(connectionError)
+                        }
                     }
                 case .failure(let error):
                     print("Error when request to upload image: " + error.localizedDescription)
