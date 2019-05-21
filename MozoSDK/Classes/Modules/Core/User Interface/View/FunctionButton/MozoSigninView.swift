db2f26333cd9c26f599679dc521ebd4535a268a2
Hoang <hoang.nn@big-labs.com>
2019-05-21 14:54:14 +0700

    Update localized title for sign in and logout button.

--- MozoSDK/Classes/Modules/Core/User Interface/View/FunctionButton/MozoSigninView.swift
+++ MozoSDK/Classes/Modules/Core/User Interface/View/FunctionButton/MozoSigninView.swift
@@ -20,14 +20,14 @@ import UIKit
 //            button.backgroundColor = ThemeManager.shared.disable
             let image = UIImage(named: "ic_logout", in: BundleManager.mozoBundle(), compatibleWith: nil)
             button.setImage(image, for: .normal)
-            button.setTitle("Log Out MozoX", for: .normal)
+            button.setTitle("Log Out MozoX".localized, for: .normal)
             button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
         } else {
 //            isUserInteractionEnabled = true
 //            button.backgroundColor = ThemeManager.shared.main
             let image = UIImage(named: "ic_user_white", in: BundleManager.mozoBundle(), compatibleWith: nil)
             button.setImage(image, for: .normal)
-            button.setTitle("Sign In to MozoX", for: .normal)
+            button.setTitle("Sign In to MozoX".localized, for: .normal)
             button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -6, bottom: 0, right: 0)
         }
         button.backgroundColor = ThemeManager.shared.main
