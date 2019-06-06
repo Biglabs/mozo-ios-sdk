f835c25b216f5c4d3d04162e1d730f093a9ede26
Hoang <hoang.nn@big-labs.com>
2019-06-06 14:14:55 +0700

    Add more logs.

--- MozoSDK/Classes/Modules/Maintenance/User Interface/Wireframe/DisplayUtils+Maintenance.swift
+++ MozoSDK/Classes/Modules/Maintenance/User Interface/Wireframe/DisplayUtils+Maintenance.swift
@@ -9,17 +9,23 @@ import Foundation
 import AppAuth
 extension DisplayUtils {
     public static func displayMaintenanceScreen() {
+        print("DisplayUtils - Display maintenance screen")
         if let topViewController = getTopViewController() {
             if topViewController is MaintenanceViewController {
-                print("Maintenence screen is being displayed.")
+                print("DisplayUtils - Maintenence screen is being displayed.")
                 return
             }
             if topViewController.isKind(of: NSClassFromString("SFAuthenticationViewController")!) {
-                print("Authentication screen is being displayed.")
+                print("DisplayUtils - Authentication screen is being displayed.")
                 return
             }
+            print("DisplayUtils - topViewController - presentingViewController: \(String(describing: topViewController.presentingViewController))")
+            print("DisplayUtils - topViewController - presentedViewController: \(String(describing: topViewController.presentedViewController))")
+            print("DisplayUtils - Display maintenance screen with top view controller: \(topViewController)")
             let viewController = MaintenanceViewController.viewControllerFromStoryboard()
             topViewController.present(viewController, animated: false)
+        } else {
+            print("DisplayUtils - Can not display maintenance screen - no top view controller")
         }
     }
 }
