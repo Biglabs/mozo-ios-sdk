cf4840010073246219f3814cefb2111d690ff470
Hoang <hoang.nn@big-labs.com>
2019-06-12 09:29:55 +0700

    Refactor code. Support multiple storyboard.

--- MozoSDK/Classes/Modules/Core/User Interface/Base/MozoWireframe.swift
+++ MozoSDK/Classes/Modules/Core/User Interface/Base/MozoWireframe.swift
@@ -15,24 +15,18 @@ class MozoWireframe: NSObject {
     }
     
     func presentWaitingInterface(corePresenter: CorePresenter?) {
-        let viewController = waitingViewControllerFromStoryboard()
+        let viewController = viewControllerFromStoryBoard(WaitingViewControllerIdentifier) as! WaitingViewController
         corePresenter?.waitingViewInterface = viewController
         viewController.eventHandler = corePresenter
         rootWireframe?.showRootViewController(viewController, inWindow: (UIApplication.shared.delegate?.window!)!)
-    }
-    
-    func waitingViewControllerFromStoryboard() -> WaitingViewController {
-        let storyboard = StoryboardManager.mozoStoryboard()
-        let viewController = storyboard.instantiateViewController(withIdentifier: WaitingViewControllerIdentifier) as! WaitingViewController
-        return viewController
     }
     
     public func getTopViewController() -> UIViewController! {
         return rootWireframe?.getTopViewController()
     }
     
-    public func viewControllerFromStoryBoard(_ identifier: String) -> UIViewController {
-        let storyboard = StoryboardManager.mozoStoryboard()
+    public func viewControllerFromStoryBoard(_ identifier: String, storyboardName: String = "") -> UIViewController {
+        let storyboard = StoryboardManager.mozoStoryboard(name: storyboardName)
         let viewController = storyboard.instantiateViewController(withIdentifier: identifier)
         return viewController
     }
