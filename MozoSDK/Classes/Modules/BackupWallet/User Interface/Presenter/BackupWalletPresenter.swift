42def7718e279a9a40ae677caa5a0af4552d275f
Hoang <hoang.nn@big-labs.com>
2019-06-17 10:43:13 +0700

    Implement Backup Wallet.

--- MozoSDK/Classes/Modules/BackupWallet/User Interface/Presenter/BackupWalletPresenter.swift
+++ MozoSDK/Classes/Modules/BackupWallet/User Interface/Presenter/BackupWalletPresenter.swift
@@ -13,6 +13,35 @@ class BackupWalletPresenter: NSObject {
     var delegate: BackupWalletModuleDelegate?
     
     var requestedModule: Module = .Wallet
+    
+    func checkRememberPIN() {
+        if let wallet = SessionStoreManager.loadCurrentUser()?.profile?.walletInfo,
+            let encryptedMnemonics = wallet.encryptSeedPhrase {
+            if let encryptedPin = wallet.encryptedPin, let pinSecret = AccessTokenManager.getPinSecret() {
+                let decryptPin = encryptedPin.decrypt(key: pinSecret)
+                let mnemonics = encryptedMnemonics.decrypt(key: decryptPin)
+                self.displayBackupWallet(mnemonics: mnemonics)
+            } else {
+                wireframe?.presentPINInterface()
+            }
+        } else {
+            // System error
+        }
+    }
+    
+    func displayBackupWallet(mnemonics: String) {
+        wireframe?.presentPassPhraseInterface(mnemonics: mnemonics)
+    }
+    
+    func processAfterReceivingPIN(pin: String) {
+        if let wallet = SessionStoreManager.loadCurrentUser()?.profile?.walletInfo,
+            let encryptedMnemonics = wallet.encryptSeedPhrase {
+            let mnemonics = encryptedMnemonics.decrypt(key: pin)
+            self.displayBackupWallet(mnemonics: mnemonics)
+        } else {
+            // System error
+        }
+    }
 }
 extension BackupWalletPresenter: BackupWalletModuleInterface {
     func verifyPassPhrases(_ passPhrases: String, indexs: [Int], originalPassPhrases: String) {
@@ -34,5 +63,10 @@ extension BackupWalletPresenter: BackupWalletInteractorOutput {
         } else {
             wireframe?.presentBackupWalletSuccessInterface()
         }
+    }
+}
+extension BackupWalletPresenter: PinModuleDelegate {
+    func verifiedPINSuccess(_ pin: String) {
+        processAfterReceivingPIN(pin: pin)
     }
 }
