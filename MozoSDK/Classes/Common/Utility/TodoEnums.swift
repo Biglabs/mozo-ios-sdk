16cf6cca4ea54dcb973000b93242bb3115c6ec8b
Hoang <hoang.nn@big-labs.com>
2019-08-14 14:04:13 +0700

    Update todo enum messages and action texts.

--- MozoSDK/Classes/Common/Utility/TodoEnums.swift
+++ MozoSDK/Classes/Common/Utility/TodoEnums.swift
@@ -24,20 +24,39 @@ public enum TodoEnum: String {
 
     public var message: String {
         switch self {
-        case .LOCATION_SERVICE_OFF: return "Location service is off."
-        case .BLUETOOTH_OFF: return "Bluetooth is turned off."
-        case .AIRDROP_NEARBY: return "Get MozoX from nearby airdrop events."
-        case .PROMOTION_NEARBY: return "Purchased promotions nearby."
-        case .VOUCHER_NEARBY: return "Purchased promotions nearby."
-        case .VOUCHER_EXPIRED: return "Purchased promotions expiring soon."
-        case .AIRDROP_EXPIRED: return "Airdrop events that will expire soon."
-        case .LOW_MOZOX_RETAILER: return "Your wallet is almost out of MozoX."
-        case .LOW_MOZOX_SHOPPER: return "Your wallet is almost out of MozoX."
-        case .PROMOTION_EXPIRED: return "Promotions that will expire soon."
-        case .AIRDROP_EMPTY: return "No planned airdrop events"
-        case .PROMOTION_EMPTY: return "No planned promotions"
-        case .UNSECURE_WALLET: return "Unsecure wallet"
-        case .AIRDROP_OUT_OF_MOZOX: return "Airdrop events is almost out of MozoX."
+        case .LOCATION_SERVICE_OFF: return "Location service is off 🗺"
+        case .BLUETOOTH_OFF: return "Bluetooth is tuned off 😔..."
+        case .AIRDROP_NEARBY: return "Get MozoX from nearby airdrop events 🎁"
+        case .PROMOTION_NEARBY: return "Interesting promotions nearby 🤩"
+        case .VOUCHER_NEARBY: return "Purchased promotions nearby 🤣"
+        case .VOUCHER_EXPIRED: return "Purchased promotions expiring soon 😣"
+        case .AIRDROP_EXPIRED: return "Airdrop events that will expire soon ⏳"
+        case .LOW_MOZOX_RETAILER: return "Your wallet is almost out of MozoX 😞"
+        case .LOW_MOZOX_SHOPPER: return "Your wallet is almost out of MozoX 😞"
+        case .PROMOTION_EXPIRED: return "Promotions that will expire soon 👌"
+        case .AIRDROP_EMPTY: return "No planned airdrop events 😐"
+        case .PROMOTION_EMPTY: return "No planned promotions 😐"
+        case .UNSECURE_WALLET: return "Unsecure wallet 😱"
+        case .AIRDROP_OUT_OF_MOZOX: return "Airdrop events is almost out of MozoX 😞"
+        }
+    }
+    
+    public var actionText: String {
+        switch self {
+        case .LOCATION_SERVICE_OFF: return "Turn on for better experience →"
+        case .BLUETOOTH_OFF: return "Turn on bluetooth to receive MozoX →"
+        case .AIRDROP_NEARBY: return "Discover stores offering MozoX →"
+        case .PROMOTION_NEARBY: return "Discover your bookmarked promotions →"
+        case .VOUCHER_NEARBY: return "Discover your purchased promotions →"
+        case .VOUCHER_EXPIRED: return "Discover your purchased promotions →"
+        case .AIRDROP_EXPIRED: return "Review and create a new airdrop event →"
+        case .LOW_MOZOX_RETAILER: return "Buy more MozoX →"
+        case .LOW_MOZOX_SHOPPER: return "Discover stores offering MozoX →"
+        case .PROMOTION_EXPIRED: return "Create a new promotion →"
+        case .AIRDROP_EMPTY: return "Create to increase foot traffic →"
+        case .PROMOTION_EMPTY: return "Create promotion →"
+        case .UNSECURE_WALLET: return "Set up your own PIN →"
+        case .AIRDROP_OUT_OF_MOZOX: return "Review and add more MozoX →"
         }
     }
 }
