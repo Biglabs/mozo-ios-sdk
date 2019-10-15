0b329284559b559b04423ecf1f26b92003968c57
Hoang <hoang.nn@big-labs.com>
2019-10-15 13:26:07 +0700

    Support parking ticket.

--- MozoSDK/Classes/Common/Model/DTO/Ticket/VehicleInfoDTO.swift
+++ MozoSDK/Classes/Common/Model/DTO/Ticket/VehicleInfoDTO.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import SwiftyJSON
+public class VehicleInfoDTO: ResponseObjectSerializable {
+    public var defaultValue: String?
+    public var items: [VehicleDTO]?
+    
+    public required init?(json: SwiftyJSON.JSON) {
+        self.defaultValue = json["defaultValue"].string
+        self.items = json["items"].array?.map({ VehicleDTO(json: $0)! })
+    }
+}
