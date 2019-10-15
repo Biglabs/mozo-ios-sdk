697ce1a3044bbb3f5eb99475b3544e78eec58fea
Hoang <hoang.nn@big-labs.com>
2019-10-15 13:25:19 +0700

    Support load GIF image.

---
+++ MozoSDK/Classes/Common/Model/DTO/Ticket/VehicleDTO.swift
@@ -0,0 +1,8 @@
+//
+//  VehicleDTO.swift
+//  MozoSDK
+//
+//  Created by Hoang Nguyen on 10/15/19.
+//
+
+import Foundation
---
+++ MozoSDK/Classes/Common/Model/DTO/Ticket/VehicleInfoDTO.swift
@@ -0,0 +1,8 @@
+//
+//  VehicleInfoDTO.swift
+//  MozoSDK
+//
+//  Created by Hoang Nguyen on 10/15/19.
+//
+
+import Foundation
