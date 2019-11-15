64a3d0cdd34c7b3077e07a1c33d6b4fb088a0b54
Hoang <hoang.nn@big-labs.com>
2019-11-15 14:52:59 +0700

    Update transaction cell.

--- MozoSDK/Classes/Modules/TxHistory/User Interface/View/TxHistoryTableViewCell.swift
+++ MozoSDK/Classes/Modules/TxHistory/User Interface/View/TxHistoryTableViewCell.swift
@@ -31,18 +31,9 @@ public class TxHistoryTableViewCell: UITableViewCell {
     func bindData(){
         lbAction.text = txHistory?.action.localized
         lbDateTime?.attributedText = txHistory?.fromNameWithDate
-        var imageName = "ic_received_circle"
+        
         let amount = txHistory?.amount ?? 0
         let amountText = amount.roundAndAddCommas()
-        if txHistory?.action == TransactionType.Received.value {
-            lbAmount.textColor = ThemeManager.shared.main
-            lbAmount.text = "+\(amountText)"
-        } else {
-            lbAmount.textColor = ThemeManager.shared.title
-            lbAmount.text = "-\(amountText)"
-            imageName = "ic_sent_circle"
-        }
-        img.image = UIImage(named: imageName, in: BundleManager.mozoBundle(), compatibleWith: nil)
         // With top up transaction, image tint color will be changed to ThemeManager.shared.success
         if let topUpReason = txHistory?.topUpReason {
             if topUpReason == .TOP_UP_ADD_MORE {
@@ -51,13 +42,31 @@ public class TxHistoryTableViewCell: UITableViewCell {
                 topUpContainerView.isHidden = false
                 topUpImageView.image = UIImage(named: "ic_receive", in: BundleManager.mozoBundle(), compatibleWith: nil)
                 topUpContainerView.backgroundColor = ThemeManager.shared.success
+                
+                lbAmount.textColor = ThemeManager.shared.main
+                lbAmount.text = "+\(amountText)"
             } else {
-                imageName = "ic_sent_circle"
+                img.isHidden = false
+                topUpContainerView.isHidden = true
+                let imageName = "ic_sent_circle"
                 img.image = UIImage(named: imageName, in: BundleManager.mozoBundle(), compatibleWith: nil)
+                
+                lbAmount.textColor = ThemeManager.shared.title
+                lbAmount.text = "-\(amountText)"
             }
+        } else {
+            var imageName = "ic_received_circle"
+            if txHistory?.action == TransactionType.Received.value {
+                lbAmount.textColor = ThemeManager.shared.main
+                lbAmount.text = "+\(amountText)"
+            } else {
+                lbAmount.textColor = ThemeManager.shared.title
+                lbAmount.text = "-\(amountText)"
+                imageName = "ic_sent_circle"
+            }
+            img.image = UIImage(named: imageName, in: BundleManager.mozoBundle(), compatibleWith: nil)
         }
         lbExchangeValue?.text = ""
 //        lbExchangeValue?.text = "₩\(txHistory?.exAmount ?? 0)"
-        
     }
 }
