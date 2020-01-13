73d6c1d008c2fc6dd6f5e8873eb4ea77b7329b60
Hoang <hoang.nn@big-labs.com>
2020-01-13 14:13:54 +0700

    Update extension to round corners.

--- MozoSDK/Classes/Common/Extension/Extensions.swift
+++ MozoSDK/Classes/Common/Extension/Extensions.swift
@@ -179,8 +179,8 @@ public extension UIView {
         clipsToBounds = true
     }
     
-    func roundCornersBezier(corners: UIRectCorner, radius: CGFloat) {
-        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+    func roundCornersBezier(frame: CGRect, corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: frame, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         let mask = CAShapeLayer()
         mask.path = path.cgPath
         layer.mask = mask
