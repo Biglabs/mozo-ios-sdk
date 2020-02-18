291e875186c07ffd012c0cb09e3502c0d0ee2633
Hoang <hoang.nn@big-labs.com>
2020-02-18 20:10:18 +0700

    Support method to check top notch

--- MozoSDK/Classes/Common/Extension/UIDevice+Extensions.swift
+++ MozoSDK/Classes/Common/Extension/UIDevice+Extensions.swift
@@ -79,5 +79,16 @@ public extension UIDevice {
         
         return mapToDevice(identifier: identifier)
     }()
-    
+        
+    var hasTopNotch: Bool {
+        if #available(iOS 13.0, *) {
+            return UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.safeAreaInsets.top ?? 0 > 20
+        } else {
+            if #available(iOS 11.0, *) {
+                return UIApplication.shared.delegate?.window??.safeAreaInsets.top ?? 0 > 20
+            }
+        }
+
+        return false
+    }
 }
