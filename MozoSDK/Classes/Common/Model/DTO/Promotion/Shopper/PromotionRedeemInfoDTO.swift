415c035947f7412c6a3f8ce694aa53a944a30e34
vuquangnguyen-biglabs <vu.nguyen.quang@big-labs.com>
2020-12-18 09:43:33 +0700

    Add support promotion restriction
    Signed-off-by: vuquangnguyen-biglabs <vu.nguyen.quang@big-labs.com>

--- MozoSDK/Classes/Common/Model/DTO/Promotion/Shopper/PromotionRedeemInfoDTO.swift
+++ MozoSDK/Classes/Common/Model/DTO/Promotion/Shopper/PromotionRedeemInfoDTO.swift
@@ -11,10 +11,12 @@ public class PromotionRedeemInfoDTO {
     public var balanceInfoToken: TokenInfoDTO?
     public var promo: PromotionDTO?
     public var branch: BranchInfoDTO?
+    public var status: PromoStatus?
     
     public required init?(json: SwiftyJSON.JSON) {
         self.balanceInfoToken = TokenInfoDTO(json: json["balanceInfoToken"])
         self.promo = PromotionDTO(json: json["promo"])
         self.branch = BranchInfoDTO(json: json["branch"])
+        self.status = PromoStatus(json: json["status"])
     }
 }
