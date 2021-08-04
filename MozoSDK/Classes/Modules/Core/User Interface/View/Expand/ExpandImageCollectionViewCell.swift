4206bd4509605b2bb446386458d11e546390bb80
Vu Nguyen <vu.nguyen.quang@big-labs.com>
2021-08-04 09:37:20 +0700

    improve UI

--- MozoSDK/Classes/Modules/Core/User Interface/View/Expand/ExpandImageCollectionViewCell.swift
+++ MozoSDK/Classes/Modules/Core/User Interface/View/Expand/ExpandImageCollectionViewCell.swift
@@ -9,21 +9,26 @@ import Foundation
 import UIKit
 let EXPAND_IMAGE_COLLECTION_VIEW_CELL_IDENTIFIER = "ExpandImageCollectionViewCell"
 class ExpandImageCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var imageView: CustomImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     
     var imageName: String? {
         didSet {
             bindData()
         }
     }
+    var isZooming = false
+    var originalImageCenter: CGPoint?
     
     func bindData() {
+        imageView.transform = .identity
+        
         if let imageName = self.imageName, !imageName.isEmpty {
+            loadingIndicator.startAnimating()
             let url = "\(Configuration.DOMAIN_IMAGE)\(imageName)"
-//            let paramUrlString = "\(url)?width=\(Int(imageView.frame.width * UIScreen.main.scale * 2))&height=\(Int(imageView.frame.height * UIScreen.main.scale * 2))"
-//            imageView.imageUrlString = paramUrlString
-//            imageView.loadImageUsingUrlString(paramUrlString, defaultImageName: "", isShowLoading: true, isUseScreenCenter: true)
-            imageView.loadImage(url, defaultImageName: "")
+            imageView.loadImage(url, defaultImageName: "") {
+                self.loadingIndicator?.stopAnimating()
+            }
         } else {
             imageView.image = nil
         }
@@ -31,6 +36,15 @@ class ExpandImageCollectionViewCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(startZooming(_:)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(pinchGesture)
+    }
+    
+    @objc private func startZooming(_ sender: UIPinchGestureRecognizer) {
+        let scaleResult = sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)
+        guard let scale = scaleResult, scale.a > 1, scale.d > 1, scale.a < 5, scale.d < 5 else { return }
+        sender.view?.transform = scale
+        sender.scale = 1
     }
 }
